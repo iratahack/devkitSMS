c9a97595338b598895325a9a08cf678819924dc9
Craig Hackney <iratahack@digitalxfer.com>
2026-03-19 08:33:23 -0700

    Add ship sprite and palette definitions to z88dk example

---
+++ examples/hello_z88dk/ship.asm
@@ -0,0 +1,29 @@
+
+    public _spritePalette
+    public _shipSprite
+    public _leftShip
+    public _rightShip
+    public _missile
+
+    section rodata_compiler
+
+    ; Sprite palette data
+_spritePalette:
+    binary "ship.pal"
+
+    ; Ship sprite tiles
+_shipSprite:
+    binary "ship.tiles"
+
+    ; Left-facing ship sprite tiles
+_leftShip:
+    binary "left_ship.tiles"
+
+    ; Right-facing ship sprite tiles
+_rightShip:
+    binary "right_ship.tiles"
+
+    ; Missile sprite tiles
+_missile:
+    binary "missile.tiles"
+
